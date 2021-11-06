76bacead2f66676ac71bd14eeabb20774f8084e0
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2021-11-06 14:23:14 +0800

    create admin planel layouts

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    @include('admin.layouts.style')
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        @include('admin.layouts.navbar')
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        @include('admin.layouts.sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        @yield('content')
+        <!-- /.content-wrapper -->
+
+        <!-- footer -->
+        @include('admin.layouts.footer')
+        <!-- /.footer -->
+
+    </div>
+    <!-- ./wrapper -->
+    @include('admin.layouts.script')
+</body>
+
+</html>
